4878fd8eeb36d0d8dc4e3c223a7af7b32467f182
Jean-Elie <jeaneliekubembula@gmail.com>
2023-07-12 17:16:56 +0200

    Implementatio des liens vers toutes les pages

--- SiteOfficiel/urls.py
+++ SiteOfficiel/urls.py
@@ -1,9 +1,13 @@
 
 from django.urls import path
 
-from SiteOfficiel.views import IndexView
+from SiteOfficiel.views import AboutView, ContactView, IndexView, ProjetView, ServiceView
 
 
 urlpatterns = [
     path('', IndexView.as_view()),
+    path('services/', ServiceView.as_view(), name='services'),
+    path('about/', AboutView.as_view(), name='about'),
+    path('projets/', ProjetView.as_view(), name='projets'),
+    path('contact/', ContactView.as_view(), name='contact'),
 ]
--- SiteOfficiel/views.py
+++ SiteOfficiel/views.py
@@ -7,3 +7,33 @@ class IndexView(View):
 
     def get(self, request):
         return render(request, self.template_name)
+
+class IndexView(View):
+    template_name = "index.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+class AboutView(View):
+    template_name = "pages/about.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+class ServiceView(View):
+    template_name = "pages/service.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+class ProjetView(View):
+    template_name = "pages/project.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+class ContactView(View):
+    template_name = "pages/contact.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
